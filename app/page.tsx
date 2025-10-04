76d742665859b19660c353874b2d4c7d47cf1fc3
AbdullahUsama <ausama.bese22seecs@seecs.edu.pk>
2025-10-04 18:10:26 +0500

    testing merge conflict tut

--- app/page.tsx
+++ app/page.tsx
@@ -179,6 +179,7 @@ const skills = [
       </nav>
       
 {/* Hero Section */}
+{/* test */}
       <section id="hero" className={styles.hero}>
         {darkMode && (
           <div style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, zIndex: 0 }}>
